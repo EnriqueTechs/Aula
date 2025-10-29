8586d6375875a617634db0bf67007bbc3e3d8992
Enrique <emlopez2019@gmail.com>
2025-10-29 20:06:23 -0300

    chore(inicial):criar site para aula

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Esta é a primeira página do meu site." />
+    <meta name="author" content="Enrique" />
+    <meta name="keywords" content="primeira, página, site, Enrique" />
+    <title>Primeira Página</title>
+</head>
+<body>
+    Hello, World!
+</body>
+</html>
